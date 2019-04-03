da5563fd86bac42f0dbaaa5d011accfffbfea7ed
Rakshan Shetty <shetty.raxx555@gmail.com>
2019-04-03 23:50:27 +0530

    Map completed

--- basics.go
+++ basics.go
@@ -77,6 +77,93 @@ func switchcase() {
 	}
 }
 
+func array() {
+	var a [5]int
+	fmt.Println("emp: ", a)
+
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
+
+	fmt.Println("len", len(a))
+
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl:", b)
+
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d:", twoD)
+}
+
+func slices() {
+	s := make([]string, 3)
+	fmt.Println("emp", s)
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
+
+	fmt.Println("len", len(s))
+
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("apd:", s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+
+	fmt.Println("cpy:", c)
+
+	l := s[2:5]
+	fmt.Println("sl1:", l)
+
+	l = s[:5]
+	fmt.Println("sl2:", l)
+
+	l = s[2:]
+	fmt.Println("sl3:", l)
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
+}
+
+func maps() {
+	m := make(map[string]int)
+
+	m["k1"] = 1
+	m["k2"] = 2
+
+	fmt.Println("map:", m)
+
+	fmt.Println("len:", len(m))
+
+	delete(m, "k2")
+	fmt.Println("map:", m)
+
+	_, prs := m["k2"]
+	fmt.Println("prs:", prs)
+
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map:", n)
+}
+
 func main() {
 	helloworld()
 	variables()
@@ -84,4 +171,7 @@ func main() {
 	iterators()
 	ifelse()
 	switchcase()
+	array()
+	slices()
+	maps()
 }
